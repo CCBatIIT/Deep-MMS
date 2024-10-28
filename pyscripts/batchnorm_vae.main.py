5c0afb46129e25ecce3b94a28a1471903e94b537
Joseph DePaolo-Boisvert <josephdb@br011.ib.bridges2.psc.edu>
2024-10-27 22:23:01 -0400

    add batchnorm script

---
+++ pyscripts/batchnorm_vae.main.py
@@ -0,0 +1,13 @@
+from DEV_main import NN_Experiment
+import sys, jax, json, time
+import jax.numpy as jnp
+
+jax.config.update("jax_enable_x64", True)
+
+json_fn = sys.argv[1]
+
+#Init Experiment
+experiment = NN_Experiment(json_fn)
+
+#Train 
+experiment.MAIN_scale_and_train_potential(n_rmsd=2000)
