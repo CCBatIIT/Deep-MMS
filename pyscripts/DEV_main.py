2ef9d7c27dcc2c48fa0858522838509841d2b91e
josephdepaoloboisvert <exouser@image-build-instance-ubuntu-24-1729561248967190838.js2local>
2024-10-28 03:12:53 +0000

    try block for xml files

--- pyscripts/DEV_main.py
+++ pyscripts/DEV_main.py
@@ -159,7 +159,10 @@ class NN_Experiment():
                 gas_fun, _ = jaa.get_openmm_energy_functions(fname_prmtop)
                 c = md.load(fname_dcd, top=fname_prmtop)
             elif fname_prmtop.endswith('xml'):
-                import jax_openmm as jaa
+                try:
+                    import jax_openmm as jaa
+                except:
+                    from . import jax_openmm as jaa
                 gas_fun, _ = jaa.get_openmm_energy_functions(fname_prmtop)
                 c = md.load(fname_dcd, top=fname_pdb)
             else:
